88ce4659e856ddad7c17a1a82e79287f50cdbf49
PaulVirally <pvirally@gmail.com>
2017-10-19 16:31:07 -0400

    Used the new oi.getJoySpeed() and oi.getJoyTurn() functions

--- commands/smoothfollowjoystick.py
+++ commands/smoothfollowjoystick.py
@@ -17,29 +17,23 @@ class SmoothFollowJoystick(Command):
 
     def execute(self):
         # Get the values from the joystick
-        joyY = oi.joystick.getY()
-        joyX = oi.joystick.getX()
-        joyZ = oi.joystick.getZ()
+        joySpeed = oi.getJoySpeed()
+        joyTurn = oi.getJoyTurn()
 
         # Put the values through the sigmoid function
         # to smooth them out
         smoothing = 4
-        sigY = 2*utils.sigmoid(joyY, a=smoothing)-1
-        sigX = 2*utils.sigmoid(joyX, a=smoothing)-1
-        sigZ = 2*utils.sigmoid(joyZ, a=smoothing)-1
+        sigJoySpeed = 2*utils.sigmoid(joySpeed, a=smoothing)
+        sigJoyTurn = 2*utils.sigmoid(joyTurn, a=smoothing)
         s1   = 2*utils.sigmoid(1, a=smoothing)-1
         sn1  = 2*utils.sigmoid(-1, a=smoothing)-1
 
         # Make sure out speed goes from -1 to 1
-        speed = utils.remap(sigY, sn1, s1, -1, 1)
-
-        # Take the largest of either the X or the Z axis
-        # and then ensure that it goes from -1 to 1
-        turn = (joyX if abs(joyX) > abs(joyZ) else joyZ)
-        turn = utils.remap(turn, sn1, s1, -1, 1)
+        speed = utils.remap(sigJoySpeed, sn1, s1, -1, 1)
+        turn = utils.remap(sigJoyTurn, sn1, s1, -1, 1)
 
         # Little bit of debugging
-        print('({0}, {1}) -> ({2}, {3})'.format(joyX, joyY, turn, speed))
+        print('({0}, {1}) -> ({2}, {3})'.format(joySpeed, joyTurn, speed, turn))
 
         # Drive
         subsystems.drivetrain.drive(speed, turn)
