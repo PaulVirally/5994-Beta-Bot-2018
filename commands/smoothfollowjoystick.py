3d00caf58bc57914cbc3205fb292ebb3bf134373
PaulVirally <pvirally@gmail.com>
2017-10-19 16:06:01 -0400

    Made sure 0 input on joystick is 0 output, not -1

--- commands/smoothfollowjoystick.py
+++ commands/smoothfollowjoystick.py
@@ -24,11 +24,11 @@ class SmoothFollowJoystick(Command):
         # Put the values through the sigmoid function
         # to smooth them out
         smoothing = 4
-        sigY = utils.sigmoid(joyY, a=smoothing)
-        sigX = utils.sigmoid(joyX, a=smoothing)
-        sigZ = utils.sigmoid(joyZ, a=smoothing)
-        s1   = utils.sigmoid(1, a=smoothing)
-        sn1  = utils.sigmoid(-1, a=smoothing)
+        sigY = 2*utils.sigmoid(joyY, a=smoothing)-1
+        sigX = 2*utils.sigmoid(joyX, a=smoothing)-1
+        sigZ = 2*utils.sigmoid(joyZ, a=smoothing)-1
+        s1   = 2*utils.sigmoid(1, a=smoothing)-1
+        sn1  = 2*utils.sigmoid(-1, a=smoothing)-1
 
         # Make sure out speed goes from -1 to 1
         speed = utils.remap(sigY, sn1, s1, -1, 1)
