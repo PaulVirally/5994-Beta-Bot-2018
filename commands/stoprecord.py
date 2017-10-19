4c33643455451ab423f8c82f3668f63f145ef164
PaulVirally <pvirally@gmail.com>
2017-10-19 18:57:02 -0400

    Added a stoprecord command

--- commands/stoprecord.py
+++ commands/stoprecord.py
@@ -1,19 +1,17 @@
-from wpilib.command import Command
+from wpilib.command import InstantCommand
 
 import oi
 
-class Record(Command):
+class Record(InstantCommand):
     '''
     Stops recording all output from all subsystems and saves
     it all to a specified file
     '''
 
     def __init__(self, filePath):
-        super().__init__('Record')
+        super().__init__('Stop Record')
+        self.filePath = filePath
+
+    def initialize(self):
         oi.shouldRecord = False
-
-    def execute(self):
-        pass
-
-    def stop(self):
-        pass
+        subsystems.writeOutput(self.filePath)
