75cca9b7cdddc86f0a1fbfa273620b2d71c263f5
PaulVirally <pvirally@gmail.com>
2017-10-19 16:32:40 -0400

    Added a todo for making the smoothing an input that can be changed on the smart dashboard

--- commands/smoothfollowjoystick.py
+++ commands/smoothfollowjoystick.py
@@ -22,7 +22,7 @@ class SmoothFollowJoystick(Command):
 
         # Put the values through the sigmoid function
         # to smooth them out
-        smoothing = 4
+        smoothing = 4 # TODO: Put this on the smart dashboard??
         sigJoySpeed = 2*utils.sigmoid(joySpeed, a=smoothing)
         sigJoyTurn = 2*utils.sigmoid(joyTurn, a=smoothing)
         s1   = 2*utils.sigmoid(1, a=smoothing)-1
