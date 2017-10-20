84b4de5d7d988bbd005a84f5277259dbc20725f5
PaulVirally <pvirally@gmail.com>
2017-10-20 12:24:19 -0400

    Added TODO stuff

--- oi.py
+++ oi.py
@@ -3,6 +3,14 @@ from wpilib.joystick import Joystick
 from wpilib.buttons.joystickbutton import JoystickButton
 
 from commands.Brake import Brake
+# from commands.LockStraight import LockStraight
+# from commands.ReverseDrive import ReverseDrive
+# from commands.Climb import Climb
+# from commands.HoldClimb import HoldClimb
+# from commands.Drop import Drop
+# from commands.PreciseDriveWithJoystick import PreciseDriveWithJoystick
+
+import RobotMap
 
 joystick = None
 
@@ -16,8 +24,34 @@ def init():
 
     joystick = Joystick(0)
 
-    trigger = JoystickButton(joystick, Joystick.ButtonType.kTrigger)
-    trigger.whenPressed(Brake())
+    brakeButton = JoystickButton(joystick, RobotMap.buttons.brake)
+    brakeButton.whileHeld(Brake())
+
+    # TODO:
+    # lockStraightButton = JoystickButton(joystick, RobotMap.buttons.lockStraight)
+    # lockStraightButton.whileHeld(LockStraight())
+    # 
+    # reverseDriveButton = joystick(joystick, RobotMap.buttons.reverseDrive)
+    # reverseDriveButton.whileHeld(ReverseDrive())
+    #
+    # climbButton = JoystickButton(joystick, RobotMap.buttons.climb)
+    # climbButton.whileHeld(Climb())
+    # climbButton.whenReleased(HoldClimb())
+    #
+    # stopClimbButton = JoystickButton(joystick, RobotMap.buttons.stopClimb)
+    # stopClimbButton.whenPressed(StopClimb)
+    #
+    # dropButton = JoystickButton(joystick, RobotMap.buttons.drop)
+    # dropButton.whileHeld(Drop())
+    #
+    # preciseDriveButton = JoystickButton(joystick, RobotMap.buttons.preciseDrive)
+    # preciseDriveButton.whileHeld(PreciseDriveWithJoystick())
+
+    centerRecordButton = JoystickButton(joystick, RobotMap.buttons.recordCenterAuto)
+    centerRecordButton.whenPressed(Record('center_auto.auto'))
+
+    leftRecordButton = JoystickButton(joystick, RobotMap.buttons.recordLeftAuto)
+    leftRecordButton.whenPressed(Record('left_auto.auto'))
 
 def getJoyTurn():
     joyX = joystick.getX()
