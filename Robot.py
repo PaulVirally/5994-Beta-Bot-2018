babd1382633085647e0e46fadaf6ea5819b1e9ac
PaulVirally <pvirally@gmail.com>
2017-10-26 16:38:58 -0400

    Implemented a very dysfunctional simulation

--- Robot.py
+++ Robot.py
@@ -81,4 +81,4 @@ class Robot(CommandBasedRobot):
         OI.log()
 
 if __name__ == '__main__':
-    wpilib.run(Robot)
+    wpilib.run(Robot, physics_enabled=True)
---
+++ physics.py
@@ -0,0 +1,18 @@
+from pyfrc.physics import drivetrains
+
+class PhysicsEngine:
+    '''
+    Simulates a 4-wheel robot
+    '''
+
+    def __init__(self, physics_controller):
+        self.physics_controller = physics_controller
+
+    def update_sim(self, hal_data, now, dt):
+        lr_motor = hal_data['pwm'][1]['value']
+        rr_motor = hal_data['pwm'][2]['value']
+        lf_motor = hal_data['pwm'][3]['value']
+        rf_motor = hal_data['pwm'][4]['value']
+
+        speed, rotation = drivetrains.four_motor_drivetrain(lr_motor, rr_motor, lf_motor, rf_motor)
+        self.physics_controller.drive(speed, rotation, dt)
