879eac492ab22e710ab928990cf6944d7375527c
PaulVirally <pvirally@gmail.com>
2017-10-26 17:25:16 -0400

    Typo

--- Utils.py
+++ Utils.py
@@ -9,9 +9,9 @@ def remap(x, min_x, max_x, min_y, max_y):
     `max_y`.
     '''
     min_x = (min_x if min_x <= max_x else max_x)
-    max_x = (max_x if mx_x >= min_x else min_x)
+    max_x = (max_x if max_x >= min_x else min_x)
 
     min_y = (min_y if min_y <= max_y else max_y)
     max_y = (max_y if max_y >= min_y else min_y)
-    
+
     return min_y + (max_y - min_y) * ((x - min_x) / (max_x - min_x))
