b9dc6f00632dd7d47500207b866ddaa71b2f3302
PaulVirally <pvirally@gmail.com>
2017-10-26 17:36:33 -0400

    Fixed non varying input bug

--- Utils.py
+++ Utils.py
@@ -8,11 +8,15 @@ def remap(x, min_x, max_x, min_y, max_y):
     to a new number which can range from `min_y` to 
     `max_y`.
     '''
-    min_x = (min_x if min_x <= max_x else max_x)
-    max_x = (max_x if max_x >= min_x else min_x)
+    
+    tmp_min_x = (min_x if (min_x <= max_x) else max_x)
+    tmp_max_x = (max_x if (max_x >= min_x) else min_x)
 
-    min_y = (min_y if min_y <= max_y else max_y)
-    max_y = (max_y if max_y >= min_y else min_y)
+    tmp_min_y = (min_y if (min_y <= max_y) else max_y)
+    tmp_max_y = (max_y if (max_y >= min_y) else min_y)
+
+    min_x, max_x = tmp_min_x, tmp_max_x
+    min_y, max_y = tmp_min_y, tmp_max_y
 
     try:
         return min_y + (max_y - min_y) * ((x - min_x) / (max_x - min_x))
