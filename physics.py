3dba3528ef3042434b2409cb5b564694de2c771c
PaulVirally <pvirally@gmail.com>
2017-10-26 16:42:10 -0400

    Fixed the physics problem

--- physics.py
+++ physics.py
@@ -9,10 +9,10 @@ class PhysicsEngine:
         self.physics_controller = physics_controller
 
     def update_sim(self, hal_data, now, dt):
-        lr_motor = hal_data['pwm'][1]['value']
-        rr_motor = hal_data['pwm'][2]['value']
-        lf_motor = hal_data['pwm'][3]['value']
-        rf_motor = hal_data['pwm'][4]['value']
+        lr_motor = hal_data['pwm'][2]['value']
+        rr_motor = hal_data['pwm'][3]['value']
+        lf_motor = hal_data['pwm'][0]['value']
+        rf_motor = hal_data['pwm'][1]['value']
 
         speed, rotation = drivetrains.four_motor_drivetrain(lr_motor, rr_motor, lf_motor, rf_motor)
         self.physics_controller.drive(speed, rotation, dt)
