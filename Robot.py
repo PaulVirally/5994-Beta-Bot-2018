550a7b8b2c1b6b5b1b62f638947d6af105e46aa9
PaulVirally <pvirally@gmail.com>
2017-11-04 10:02:46 -0400

    Fixed typo in docstring

--- Robot.py
+++ Robot.py
@@ -76,7 +76,7 @@ class Robot(CommandBasedRobot):
 
     def log(self):
         '''Logs all the information from the subsystems and
-        from the OI to the DashBoard.'''
+        from the OI to the SmartDashoard.'''
         subsystems.log()
         OI.log()
 
