86e22898c2b5f119868a6b0d9de47be898da856f
PaulVirally <pvirally@gmail.com>
2018-01-24 12:59:01 -0500

    Added CAN support for th physics simulator (Still has problem with CAN bus index 0 not showing any indication of change in value...) Also still has the annoying which wheel is which and which wheel wshoudl be inverted problem...

--- physics.py
+++ physics.py
@@ -9,10 +9,10 @@ class PhysicsEngine:
         self.physics_controller = physics_controller
 
     def update_sim(self, hal_data, now, dt):
-        front_left_motor = hal_data['pwm'][0]['value']
-        front_right_motor = hal_data['pwm'][1]['value']
-        rear_left_motor = hal_data['pwm'][2]['value']
-        rear_right_motor = hal_data['pwm'][3]['value']
+        front_left_motor = hal_data['CAN'][0]['value']
+        front_right_motor = hal_data['CAN'][1]['value']
+        rear_left_motor = hal_data['CAN'][2]['value']
+        rear_right_motor = hal_data['CAN'][3]['value']
 
         speed, rotation = drivetrains.four_motor_drivetrain(rear_left_motor, rear_right_motor, 
                                                             front_left_motor, front_right_motor)
