04271b6a8f4d7b3ccd9ab22d80638ff98a7ab0f9
PaulVirally <pvirally@gmail.com>
2018-02-08 10:43:48 -0500

    Added 30 fps limit. The bandwidth is very close to 7mpbs. We can't really improve the quality much more without rekt by FRC.

--- vision.py
+++ vision.py
@@ -1,4 +1,5 @@
 from cscore import CameraServer
+# from cscore.VideoMode.PixelFormat import kYUYV # kMJPEG also exists
 
 def main():
     cs = CameraServer.getInstance()
@@ -7,7 +8,13 @@ def main():
     frontCam = cs.startAutomaticCapture(dev=0)
     backCam = cs.startAutomaticCapture(dev=1)
 
+    # For a list of possible combinations: http://roborio-5994-frc.local:1181/
     frontCam.setResolution(320, 240)
-    backCam.setResolution(320, 240)    
+    frontCam.setFPS(30)
+    # backCam.setPixelFormat(kYUYV)
+
+    backCam.setResolution(320, 240)
+    backCam.setFPS(30)
+    # backCam.setPixelFormat(kYUYV)
 
     cs.waitForever()
