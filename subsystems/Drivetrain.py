d2b78268131de96367009096e9f302e120e8fdb5
PaulVirally <pvirally@gmail.com>
2018-02-14 16:07:39 -0500

    Original Gyro Functionnality

--- subsystems/Drivetrain.py
+++ subsystems/Drivetrain.py
@@ -73,13 +73,13 @@ class Drivetrain(Subsystem):
         return self.lastRotateValue
 
     def getAngle(self):
-        return self.angleAcc
-        # return self.gyro.getAngle()
+        # return self.angleAcc
+        return self.gyro.getAngle()
 
     def resetGyro(self):
-        self.anglePreviousTime = time.time()
-        self.angleAcc = 0
-        # self.gyro.reset()
+        # self.anglePreviousTime = time.time()
+        # self.angleAcc = 0
+        self.gyro.reset()
 
     def getRotationRate(self):
         return self.gyro.getRate()
@@ -132,7 +132,7 @@ class Drivetrain(Subsystem):
 
     def update(self):
         self.updateRevolutionCounter()
-        self.updateAngleAcc()
+        # self.updateAngleAcc()
 
     def log(self):
         wpilib.SmartDashboard.putNumber('Speed Output', self.getSpeed())
