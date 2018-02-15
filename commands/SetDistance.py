68387e8eac66410d241871c1ad7c0873534d0025
PaulVirally <pvirally@gmail.com>
2018-02-14 21:55:01 -0500

    Added SetDistance

--- commands/SetDistance.py
+++ commands/SetDistance.py
@@ -1,6 +1,7 @@
 from wpilib.command import Command
 import wpilib
 import subsystems
+from math import pi
 
 class SetDistance(Command):
     '''
@@ -12,16 +13,31 @@ class SetDistance(Command):
 
         self.requires(subsystems.drivetrain)
         self.targetDistance = target_distance
+        self.errorCounter = 0
+
+    def getError(self):
+        return self.targetDistance - subsystems.drivetrain.getRevolutions()*pi*15.24
 
     def initialize(self):
-        pass
+        subsystems.drivetrain.resetRevolutionCounter()
         
     def execute(self):
-        pass
+        error = self.getError()
+
+        propK = 0.4/50
+
+        adjust = propK * error
+
+        if abs(adjust) >= 1:
+            adjust = 1 if adjust > 0 else -1
+
+        subsystems.drivetrain.drive(adjust, 0)
 
     def stop(self):
         subsystems.drivetrain.stop()
 
     def isFinished(self):
-        self.stop()
-        return True
+        if self.errorCounter >= 3:
+            self.stop
+            return True
+        return False
