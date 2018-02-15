6743da59e2fbbf6cd1445febb5d36aaf246c77ba
PaulVirally <pvirally@gmail.com>
2018-02-15 11:13:40 -0500

    Added the magic, needs to be tested

--- commands/SetDistance.py
+++ commands/SetDistance.py
@@ -2,6 +2,7 @@ from wpilib.command import Command
 import wpilib
 import subsystems
 from math import pi
+import time
 
 class SetDistance(Command):
     '''
@@ -14,12 +15,15 @@ class SetDistance(Command):
         self.requires(subsystems.drivetrain)
         self.targetDistance = target_distance
         self.errorCounter = 0
+        self.prevAdjust = 0
+        self.startTime = 0
 
     def getError(self):
         return self.targetDistance - subsystems.drivetrain.getRevolutions()*pi*15.24
 
     def initialize(self):
         subsystems.drivetrain.resetRevolutionCounter()
+        self.startTime = time.time()
         
     def execute(self):
         error = self.getError()
@@ -28,8 +32,22 @@ class SetDistance(Command):
 
         adjust = propK * error
 
+        # Magic
+        if abs(adjust - self.prevAdjust) < 0.1:
+            adjust = self.prevAdjust + (0.11 if adjust > 0 else -0.11)
+
         if abs(adjust) >= 1:
             adjust = 1 if adjust > 0 else -1
+
+        if abs(self.getError()) <= 5:
+            self.errorCounter += 1
+        else:
+            self.errorCounter = 0
+
+        # Don't take over 1.5 seconds
+        # if (time.time() - self.startTime) > 2:
+        #     self.shouldEndCount = 101
+        #     self.stop()
 
         subsystems.drivetrain.drive(adjust, 0)
 
