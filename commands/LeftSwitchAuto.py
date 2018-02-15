b8a95fa0f24ca643c1df1f1ee526676c1d873588
PaulVirally <pvirally@gmail.com>
2018-02-15 18:34:49 -0500

    Added 0.05 second wait times between commands

--- commands/LeftSwitchAuto.py
+++ commands/LeftSwitchAuto.py
@@ -1,4 +1,5 @@
 from wpilib.command.commandgroup import CommandGroup
+from wpilib.command.waitcommand import WaitCommand
 import wpilib
 from commands.SetDistance import SetDistance
 from commands.SetGyroAngle import SetGyroAngle
@@ -21,9 +22,11 @@ class LeftSwitchAuto(CommandGroup):
             self.addSequential(SetDistance(399.4))
 
             # Turn right
+            self.addSequential(WaitCommand(timeout=0.05))            
             self.addSequential(SetGyroAngle(90))
 
             # Drive right up to the switch
+            self.addSequential(WaitCommand(timeout=0.05))            
             self.addSequential(SetDistance(8.39))
 
             # Drop off the cube
@@ -35,21 +38,27 @@ class LeftSwitchAuto(CommandGroup):
             self.addSequential(SetDistance(137.05))
 
             # Turn right
+            self.addSequential(WaitCommand(timeout=0.05))            
             self.addSequential(SetGyroAngle(90))
 
             # Set up for going to switch
+            self.addSequential(WaitCommand(timeout=0.05))            
             self.addSequential(SetDistance(576.61))
 
             # Turn left to go to switch
+            self.addSequential(WaitCommand(timeout=0.05))            
             self.addSequential(SetGyroAngle(-90))
 
             # Go up to switch
+            self.addSequential(WaitCommand(timeout=0.05))            
             self.addSequential(SetDistance(262.35))
 
             # Turn left to face switch
+            self.addSequential(WaitCommand(timeout=0.05))            
             self.addSequential(SetGyroAngle(-90))
 
             # Drive right up to the switch
+            self.addSequential(WaitCommand(timeout=0.05))            
             self.addSequential(SetDistance(8.39))
 
             # Drop off the cube
--- commands/RightSwitchAuto.py
+++ commands/RightSwitchAuto.py
@@ -1,4 +1,5 @@
 from wpilib.command.commandgroup import CommandGroup
+from wpilib.command.waitcommand import WaitCommand
 import wpilib
 from commands.SetDistance import SetDistance
 from commands.SetGyroAngle import SetGyroAngle
@@ -21,21 +22,27 @@ class RightSwitchAuto(CommandGroup):
             self.addSequential(SetDistance(137.05))
 
             # Go turn left
+            self.addSequential(WaitCommand(timeout=0.05))            
             self.addSequential(SetGyroAngle(-90))
 
             # Set up for going to switch
+            self.addSequential(WaitCommand(timeout=0.05))            
             self.addSequential(SetDistance(576.61))
 
             # Turn right to go to switch
+            self.addSequential(WaitCommand(timeout=0.05))            
             self.addSequential(SetGyroAngle(-90))
 
             # Go up to switch
+            self.addSequential(WaitCommand(timeout=0.05))            
             self.addSequential(SetDistance(262.35))
 
             # Turn right to face switch
+            self.addSequential(WaitCommand(timeout=0.05))            
             self.addSequential(SetGyroAngle(90))
 
             # Drive right up to the switch
+            self.addSequential(WaitCommand(timeout=0.05))            
             self.addSequential(SetDistance(8.39))
 
             # Drop off the cube
@@ -44,12 +51,15 @@ class RightSwitchAuto(CommandGroup):
             # Go to right switch
 
             # Go to switch
+            self.addSequential(WaitCommand(timeout=0.05))            
             self.addSequential(SetDistance(399.4))
 
             # Turn left
+            self.addSequential(WaitCommand(timeout=0.05))            
             self.addSequential(SetGyroAngle(-90))
 
             # Drive right up to the switch
+            self.addSequential(WaitCommand(timeout=0.05))            
             self.addSequential(SetDistance(8.39))
 
             # Drop off the cube
